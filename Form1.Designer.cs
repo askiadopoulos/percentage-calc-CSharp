b25818442b6150f27e8c847fc3dcd7b6cdd7852e
Angelos Skiadopoulos <38656524+askiadopoulos@users.noreply.github.com>
2018-04-25 15:34:52 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace getPercentage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInit = new System.Windows.Forms.TextBox();
+            this.txtPct = new System.Windows.Forms.TextBox();
+            this.txtNetPct = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInit
+            // 
+            this.txtInit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.txtInit.Location = new System.Drawing.Point(241, 42);
+            this.txtInit.Name = "txtInit";
+            this.txtInit.Size = new System.Drawing.Size(134, 26);
+            this.txtInit.TabIndex = 0;
+            this.txtInit.TextChanged += new System.EventHandler(this.txtInit_TextChanged);
+            // 
+            // txtPct
+            // 
+            this.txtPct.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.txtPct.Location = new System.Drawing.Point(241, 76);
+            this.txtPct.Name = "txtPct";
+            this.txtPct.Size = new System.Drawing.Size(134, 26);
+            this.txtPct.TabIndex = 1;
+            this.txtPct.TextChanged += new System.EventHandler(this.txtPct_TextChanged);
+            // 
+            // txtNetPct
+            // 
+            this.txtNetPct.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.txtNetPct.Location = new System.Drawing.Point(229, 28);
+            this.txtNetPct.Name = "txtNetPct";
+            this.txtNetPct.ReadOnly = true;
+            this.txtNetPct.Size = new System.Drawing.Size(134, 26);
+            this.txtNetPct.TabIndex = 2;
+            this.txtNetPct.TextChanged += new System.EventHandler(this.txtNetPct_TextChanged);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(223, 257);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(85, 31);
+            this.btnCalc.TabIndex = 3;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(314, 257);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(85, 31);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.txtResult.Location = new System.Drawing.Point(229, 63);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(134, 26);
+            this.txtResult.TabIndex = 5;
+            this.txtResult.TextChanged += new System.EventHandler(this.txtResult_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(33, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1. Enter initial number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(33, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2. Enter percentage (in %):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label3.Location = new System.Drawing.Point(21, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "3. Net percentage:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label4.Location = new System.Drawing.Point(21, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "4. Final result:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(387, 110);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data Input";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtResult);
+            this.groupBox2.Controls.Add(this.txtNetPct);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 131);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(387, 110);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Calculation Results";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "made with Visual C#";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 302);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtPct);
+            this.Controls.Add(this.txtInit);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Percentage Calculator - Puplic Project (2018)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInit;
+        private System.Windows.Forms.TextBox txtPct;
+        private System.Windows.Forms.TextBox txtNetPct;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace getPercentage
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnCalc.Enabled = false;
+            btnClear.Enabled = false;
+            txtPct.Enabled = false;
+
+            txtNetPct.ReadOnly = true;
+            txtNetPct.Enabled = false;
+            txtResult.ReadOnly = true;
+            txtResult.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            float init, pct, netPct, result;
+
+            init = Convert.ToSingle(txtInit.Text);
+            pct = Convert.ToSingle(txtPct.Text);
+
+            netPct = (init * pct) / 100;
+            result = init + netPct;
+
+            txtNetPct.Enabled = true;
+            txtNetPct.Text = Convert.ToString(netPct);
+            txtResult.Enabled = true;
+            txtResult.Text = Convert.ToString(result);
+
+            btnCalc.Enabled = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInit.Clear();
+
+            txtPct.Clear();
+            txtPct.Enabled = false;
+            txtNetPct.Clear();
+            txtNetPct.Enabled = false;
+            txtResult.Clear();
+            txtResult.Enabled = false;
+
+            btnClear.Enabled = false;
+        }
+
+        private void txtInit_TextChanged(object sender, EventArgs e)
+        {
+            txtNetPct.Clear();
+            txtNetPct.Enabled = false;
+            txtResult.Clear();
+            txtResult.Enabled = false;
+
+            if (txtInit.Text.Length < 1)
+            {
+                if(txtPct.Text.Length < 1)
+                {
+                    txtPct.Enabled = false;
+                    btnClear.Enabled = false;
+                }
+                txtNetPct.Clear();
+                txtResult.Clear();
+
+                btnCalc.Enabled = false;
+            }
+            else if(!txtInit.Equals(txtInit.Text) && txtPct.Text.Length > 0)
+            {
+                btnCalc.Enabled = true;
+            }
+            else
+            {
+                btnClear.Enabled = true;
+                txtPct.Enabled = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            const string message = "Are you sure you want to quit ?";
+            const string caption = "Percentage Calculator";
+
+            var dialog = MessageBox.Show(message, caption, MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if (dialog == DialogResult.Cancel)
+                e.Cancel = true;
+        }
+
+        private void txtPct_TextChanged(object sender, EventArgs e)
+        {
+            txtNetPct.Clear();
+            txtNetPct.Enabled = false;
+            txtResult.Clear();
+            txtResult.Enabled = false;
+
+            if (txtPct.Text.Length < 1)
+            {
+                if (txtInit.Text.Length < 1)
+                {
+                    txtPct.Enabled = false;
+                    btnClear.Enabled = false;
+                }
+                txtNetPct.Clear();
+                txtResult.Clear();
+
+                btnCalc.Enabled = false;
+            }
+            else if (!txtInit.Equals(txtInit.Text) && txtInit.Text.Length < 1)
+            {
+                btnCalc.Enabled = false;
+            }
+            else
+            {
+                btnCalc.Enabled = true;
+                btnClear.Enabled = true;
+            }
+        }
+
+        private void txtNetPct_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
